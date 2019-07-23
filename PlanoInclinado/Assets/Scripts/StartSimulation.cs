da14802c503bf26c24a13c95b5febf431c99803f
Lucas Novais <lucasnovais222@gmail.com>
2019-07-23 10:32:13 -0300

    Algumas atualizacoes na simulacao do Plano Inclinado

---
+++ PlanoInclinado/Assets/Scripts/StartSimulation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSimulation : MonoBehaviour
+{
+    public void Start()
+    {
+        Time.timeScale = 0;
+    }
+    
+    public void StartSim()
+    {
+        Time.timeScale = 1;
+    }
+}
