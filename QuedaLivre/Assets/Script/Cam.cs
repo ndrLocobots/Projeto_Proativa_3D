85d7f7a3b18b4b9d9eb1ec717d5d512d2c3c1e9d
JJader03 <jjader03@gmail.com>
2019-07-30 16:10:56 -0300

    Finalizacao do QuedaLivre

---
+++ QuedaLivre/Assets/Script/Cam.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    public Transform per;
+    public Transform cam;
+    float aux;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aux = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        switch (aux)
+        {
+            case 0:
+                cam.rotation = Quaternion.Euler(per.eulerAngles.x, per.eulerAngles.y, per.eulerAngles.z);
+                cam.position = new Vector3(per.position.x, per.position.y + 1, per.position.z);
+                break;
+            case 1:
+                cam.rotation = Quaternion.Euler(25, 90, 0);
+                cam.position = new Vector3(per.position.x - 10, per.position.y + 10, per.position.z);
+                break;
+            case 2:
+                cam.rotation = Quaternion.Euler(50, 0 , 0);
+                cam.position = new Vector3(per.position.x, per.position.y + 10, per.position.z - 10);
+                break;
+            default:
+                aux = 0;
+                break;
+
+        }
+        
+    }
+
+    public void SliderAux(float parametro)
+    {
+        aux = parametro;
+    }
+}
---
+++ QuedaLivre/Assets/Script/Cubo.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cubo : MonoBehaviour
+{
+    Rigidbody body;
+    float timer, hight, ambiente;
+    float pastHigh;
+    Vector3 StPosition0, StPosition1, StPosition2;
+    public Slider altura, ambi;
+
+    bool buttom;
+
+    /*
+     * Formulas Usadas
+     * Tempo Total (TT) = vy/5
+     * Tempo Altura Máxima (TH) = vy/10
+     * DeltaX = Vx*TT
+     * Altura Maxima = y0 + (Vy*TH) - (5*TH^2)
+     */
+
+    void Start()
+    {
+        hight = 0;
+        pastHigh = hight;
+
+        body = GetComponent<Rigidbody>();
+        body.rotation = Quaternion.Euler(0, 90, 0);
+
+        timer = 0;
+
+        StPosition0 = new Vector3(-262.4f, -6.69f, 94.7f);
+        StPosition1 = new Vector3(-262.4f, -6.69f, -6f);
+        StPosition2 = new Vector3(-262.4f, -6.69f, -106.7f);
+
+        ambi.value = 1;
+    }
+
+    void Update()
+    {
+        //Andar();
+        if (pastHigh - hight != 0)
+        {
+            buttom = false;
+            body.position = new Vector3(body.position.x, hight + StPosition0.y, body.position.z);
+            pastHigh = hight;
+        }
+
+        if (Input.GetKey("o"))
+        {
+            altura.value = 0;
+            ambi.value = 1;
+            body.position = StPosition1;
+            body.rotation = Quaternion.Euler(0, 90, 0);
+            ambiente = 1;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (buttom)
+        {
+            Pular();
+        }
+    }
+
+    void Pular()
+    {
+        if (ambiente == 0)
+        {
+            body.AddForce(Vector3.down * 20);
+            timer = hight / 20;
+        }
+        else if (ambiente == 1)
+        {
+            body.AddForce(Vector3.down * 10);
+            timer = hight / 10;
+        }
+        else
+        {
+            body.AddForce(Vector3.down * 3);
+            timer = hight / 3;
+        }
+
+    }
+    public void ClickButtom(bool b)
+    {
+        buttom = true;
+    }
+
+    public void SetHight(float h)
+    {
+        hight = h;
+    }
+
+    public void SetAmbiente(float a)
+    {
+        ambiente = a;
+        switch (a)
+        {
+            case 0:
+                altura.value = 0;
+                body.position = StPosition0;
+                break;
+            case 2:
+                altura.value = 0;
+                body.position = StPosition2;
+                break;
+            default:
+                altura.value = 0;
+                body.position = StPosition1;
+                break;
+        }
+    }
+
+    void Andar()
+    {
+
+        float translate = (Input.GetAxis("Vertical") * 10) * Time.deltaTime;
+        float rotate = (Input.GetAxis("Horizontal") * 20) * Time.deltaTime;
+
+        body.transform.Translate(0, 0, translate);
+        body.transform.Rotate(0, rotate, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       
+    }
+
+    private void OnGUI()
+    {
+        GUI.contentColor = Color.white;
+        GUI.Label(new Rect(10, 80, 200, 60), "TEMPO:");
+        GUI.Label(new Rect(100, 80, 200, 30), timer.ToString("0.00"));
+        GUI.Label(new Rect(10, 110, 200, 60), "ALTURA");
+        GUI.Label(new Rect(100, 110, 200, 30), hight.ToString("0.00"));
+    }
+}
