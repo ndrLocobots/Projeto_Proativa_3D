e2a9dc24eaa2ff8ce55f97501538fa2de8761009
Lucas Novais <lucasnovais222@gmail.com>
2019-08-04 10:33:54 -0300

    Plano inclinado quase finalizado

---
+++ PlanoInclinado/Assets/Scripts/Timer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float start;
+    private bool isStarted;
+    string minutes;
+    string seconds;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Start"))
+        {
+            isStarted = true;
+
+            start = Time.time;
+        }
+            
+        if (other.CompareTag("End"))
+        {
+            isStarted = false;
+        }
+            
+    }
+
+    void Start()
+    {
+        start = Time.time;
+    }
+
+    void Update()
+    {
+        if(isStarted)
+        {
+            float t = Time.time - start;
+            minutes = ((int)t / 60).ToString();
+            seconds = (t % 60).ToString("f2");
+
+            timerText.text = "Tempo: " + minutes + ":" + seconds;
+
+            //Imprimir os valores;
+        }
+        
+     }
+}
