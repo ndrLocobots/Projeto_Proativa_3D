2bf63d3bcdc715369b7af0e5e96c2050810feb65
Lucas Novais <lucasnovais222@gmail.com>
2019-08-26 08:45:31 -0300

    Primeira versao da simulacao Massa e Mola

---
+++ MassaEMola/Assets/Scripts/MudaComp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MudaComp : MonoBehaviour
+{
+    public GameObject massa;
+    public SpringJoint sj;
+
+    public void ChangeComp(float transf)
+    {
+        
+
+        massa.transform.position = new Vector3(massa.transform.position.x - transf, massa.transform.position.y, massa.transform.position.z);
+    }
+}
