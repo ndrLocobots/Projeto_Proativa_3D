19f45919bde7ffafa58407f572c2f7d2f90692c9
Lucas Novais <lucasnovais222@gmail.com>
2019-10-23 00:01:33 -0200

    Correcao de alguns bugs e primeiro menu implementado.

---
+++ MenuFase/Assets/Script/Menu/TransitaMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitaMenu : MonoBehaviour
+{
+    public void Carrega02()
+    {
+        SceneManager.LoadScene("Menu02");
+    }
+
+    public void Carrega01()
+    {
+        SceneManager.LoadScene("Menu01");
+    }
+
+    public void CarregaPlano()
+    {
+        SceneManager.LoadScene("Plano");
+    }
+
+    public void CarregaMassa()
+    {
+        SceneManager.LoadScene("Massa");
+    }
+
+    public void CarregaQueda()
+    {
+        SceneManager.LoadScene("Queda");
+    }
+
+    public void CarregaForca()
+    {
+        SceneManager.LoadScene("Forca");
+    }
+
+    public void CarregaLanc()
+    {
+        SceneManager.LoadScene("Lancamento");
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+    }
+}
