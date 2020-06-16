c7b78b8815a92dbf1bb16c0945c833c3805792b5
Jamisson Jader <jjader03@gmail.com>
2020-06-16 04:55:26 -0300

    adicionando rotação e posição da câmera

---
+++ MenuFase/Assets/Script/camera/position.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class position : MonoBehaviour
+{
+  public Transform personagem;
+  private Transform positionCamera;
+  float aux;
+  // Start is called before the first frame update
+  void Start()
+  {
+    aux = 0;
+    positionCamera = GetComponent<Transform>();
+  }
+
+  // Update is called once personagem frame
+  void Update()
+  {
+
+    switch (aux)
+    {
+      case 0:
+        positionCamera.position = new Vector3(personagem.position.x, personagem.position.y + 1, personagem.position.z);
+        break;
+      case 1:
+        positionCamera.position = new Vector3(personagem.position.x - 10, personagem.position.y + 10, personagem.position.z);
+        break;
+      case 2:
+        positionCamera.position = new Vector3(personagem.position.x, personagem.position.y + 10, personagem.position.z - 20);
+        break;
+      default:
+        aux = 0;
+        break;
+
+    }
+
+  }
+
+  public void SliderAux(float parametro)
+  {
+    aux = parametro;
+  }
+}
---
+++ MenuFase/Assets/Script/camera/rotation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+  // Start is called before the first frame update
+  public float sensibilidade = 2.0f;
+  private Transform positionCamera;
+  private float mouseX = 0.0f, mouseY = 0.0f;
+  private bool isMenu = false;
+  void Start()
+  {
+    positionCamera = GetComponent<Transform>();
+
+    //Cursor.visible = true;
+    Cursor.lockState = CursorLockMode.Locked;
+  }
+
+
+  void Update()
+  {
+    if (!isMenu)
+    {
+      Cursor.lockState = CursorLockMode.Locked;
+      mouseX += Input.GetAxis("Mouse X") * sensibilidade;
+      mouseY -= Input.GetAxis("Mouse Y") * sensibilidade;
+      positionCamera.eulerAngles = new Vector3(mouseY, mouseX, 0);
+    }
+
+    if (Input.GetKeyDown(KeyCode.M))
+    {
+      Cursor.visible = true;
+      Cursor.lockState = CursorLockMode.None;
+      isMenu = !isMenu;
+    }
+  }
+}
