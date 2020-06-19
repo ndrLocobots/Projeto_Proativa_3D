41287780deb9c7ed667037ced5b19e5a02ecf01c
Jamisson Jader <jjader03@gmail.com>
2020-06-19 00:05:47 -0300

    otimizando cubo lancamento vertical, deixando sem loop

--- MenuFase/Assets/Script/Lancamento/CuboLan.cs
+++ MenuFase/Assets/Script/Lancamento/CuboLan.cs
@@ -6,137 +6,49 @@ using UnityEngine.UI;
 public class CuboLan : MonoBehaviour
 {
   Rigidbody body;
-  float velocidade;
-  float angulo, anguloRad;
-  float timer, MaxHeight, DeltaX;
-  public bool jumper, buttom, restart;
-  Vector3 velo, StPosition;
-  public GameObject result;
-  public Text t, am, dx, an, vel; // tempo, altura maxima, deltax
+  public GameObject panel;
+  ScreenResults script;
+  Vector3 startingPosition;
 
-  /*
-   * Formulas Usadas
-   * Tempo Total (TT) = vy/5
-   * Tempo Altura Máxima (TH) = vy/10
-   * DeltaX = Vx*TT
-   * Altura Maxima = y0 + (Vy*TH) - (5*TH^2)
-   */
+  public bool isJumper;
 
   void Start()
   {
+    script = GetComponent<ScreenResults>();
+
     body = GetComponent<Rigidbody>();
     body.freezeRotation = true;
 
-    jumper = true;
-    timer = 0;
-    MaxHeight = 0;
-    DeltaX = 0;
-    buttom = restart = false;
+    isJumper = false;
 
-    StPosition = new Vector3(-262.4f, -9.492697f, -6f);
-    result.SetActive(false);
+    startingPosition = body.position;
+    panel.SetActive(false);
   }
 
-  void Update()
+  public void ClickStart(bool b)
   {
-
-    if (Input.GetKeyDown(KeyCode.Space) || buttom)
+    if (body.velocity.magnitude == 0)
     {
-      if (velocidade != 0)
-      {
-        if (angulo > 0 && angulo <= 90)
-        {
-          Pular(jumper);
-          jumper = false;
-          buttom = false;
-        }
-      }
-
-    }
-
-    if (Input.GetKeyDown(KeyCode.O) || restart)
-    {
-      body.velocity = new Vector3(0, 0, 0);
-      body.position = StPosition;
-      jumper = true;
-      timer = 0;
-      MaxHeight = 0;
-      DeltaX = 0;
-      result.SetActive(false);
-      restart = false;
+      body.velocity = script.SetUserVelocity();
+      isJumper = true;
     }
   }
 
-  public void ClickButtom(bool b)
+  public void ClickRestore(bool b)
   {
-    buttom = true;
-  }
-
-  public void ClickButtom1(bool b)
-  {
-    restart = true;
-  }
-
-  void Pular(bool j)
-  {
-    if (j)
-    {
-      anguloRad = Mathf.PI * angulo / 180; // transformando o angulo em radiano
-      velo.x = Mathf.Cos(anguloRad) * velocidade;
-      velo.y = Mathf.Sin(anguloRad) * velocidade;
-      velo.z = body.velocity.z;
-      body.velocity = velo;
-    }
-  }
-
-
-  float TempoTotal()
-  {
-    return velo.y / 5;
-  }
-
-  float TempoAlturaMaxima()
-  {
-    return velo.y / 10;
-  }
-
-  float DistanciaX(float t)
-  {
-    return velo.x * t;
-  }
-
-  float AlturaMaxima(float t)
-  {
-    return (velo.y * t) - (5 * t * t);
+    body.velocity = new Vector3(0, 0, 0);
+    body.position = startingPosition;
+    isJumper = false;
+    panel.SetActive(false);
   }
 
   private void OnCollisionEnter(Collision collision)
   {
-    if (!jumper)
+    if (isJumper)
     {
       body.velocity = new Vector3(0, 0, 0);
-      MaxHeight = AlturaMaxima(TempoAlturaMaxima());
-      timer = TempoTotal();
-      DeltaX = DistanciaX(timer);
-      result.SetActive(true);
+      script.SetOutputParam();
+      panel.SetActive(true);
     }
-
-  }
-  public void SetVelocidade(float v)
-  {
-    velocidade = v;
-  }
-  public void SetAngulo(float a)
-  {
-    angulo = a;
-  }
-
-  private void OnGUI()
-  {
-    an.text = angulo.ToString("0");
-    vel.text = velocidade.ToString("0");
-    t.text = "Tempo: " + timer.ToString("0.00");
-    am.text = "Altura máxima: " + MaxHeight.ToString("0.00");
-    dx.text = "Distância horizontal: " + DeltaX.ToString("0.00");
   }
 }
---
+++ MenuFase/Assets/Script/Lancamento/ScreenResults.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenResults : MonoBehaviour
+{
+  Vector3 userVelocity;
+
+  public Text Ttime, TmaxHigh, TdeltaX;
+  public Text Tangle, Tvelocity;
+
+  float totalTimer, maxHeight, DeltaX;
+  float velocity, angle;
+
+  public void SetVelocity(float velocity){
+    this.velocity = velocity;
+  }
+
+  public void SetAngle(float angle){
+    this.angle = angle;
+  }
+  
+  public Vector3 SetUserVelocity()
+  {
+    float anguloRad = Mathf.PI * angle / 180;
+
+    userVelocity.x = Mathf.Cos(anguloRad) * velocity;
+    userVelocity.y = Mathf.Sin(anguloRad) * velocity;
+
+    return userVelocity;
+  }
+
+  public void SetOutputParam(){
+    SetTotalTime();
+    SetDeltaX();
+    SetMaxHigh();
+  }
+
+  void SetTotalTime()
+  {
+    // y = y0 + v0*T + at²/2
+    // where y = y0 = 0 and a = 10
+
+    totalTimer = userVelocity.y / 5;
+  }
+
+  void SetDeltaX()
+  {
+    // x = x0 + vT
+
+    DeltaX = userVelocity.x * totalTimer;
+  }
+
+  void SetMaxHigh()
+  {
+    // y = y0 + v0*t + at²/2
+    // where y is max when t = totalTimer/2
+    
+    float time = totalTimer/2;
+    maxHeight = (userVelocity.y * time) - (5 * time * time);
+  }
+
+  private void OnGUI()
+  {
+    Tangle.text = angle.ToString("0");
+    Tvelocity.text = velocity.ToString("0");
+
+    Ttime.text = "Tempo: " + totalTimer.ToString("0.00");
+    TmaxHigh.text = "Altura máxima: " + maxHeight.ToString("0.00");
+    TdeltaX.text = "Distância horizontal: " + DeltaX.ToString("0.00");
+  }
+}
