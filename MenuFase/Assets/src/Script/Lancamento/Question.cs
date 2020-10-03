ebe594c545176f8f699f2f354fa61fb4a5d9d8e0
jjader03 <jjader03@gmail.com>
2020-10-03 01:34:21 -0300

    fazendo leitura dos arquivos dentro do question

--- MenuFase/Assets/src/Script/Lancamento/Question.cs
+++ MenuFase/Assets/src/Script/Lancamento/Question.cs
@@ -7,8 +7,8 @@ public class Question : MonoBehaviour
   public float distaceDelta;
   float velocityX, velocityY, time, maxHeight, velocity, angle;
 
-  public string[] setencas;
-
+  CSVfile csvfile = new CSVfile();
+  
   void Start()
   {
     UpadateRobotSetence();
@@ -29,7 +29,7 @@ public class Question : MonoBehaviour
   void MakeDataQuestion()
   {
     distaceDelta = 0;
-    while (distaceDelta<30 || distaceDelta>250)
+    while (distaceDelta < 30 || distaceDelta > 250)
     {
       angle = Random.Range(1, 90f);
       float RadianAngle = angle * Mathf.PI / 180;
@@ -49,38 +49,28 @@ public class Question : MonoBehaviour
 
   string[] GetSetences()
   {
-    setencas = new string[7];
+    List<string> setencas = new List<string>();
 
-    setencas[0] = "Olá gamer, meu nome é locobits !! Estou muito feliz de ter você aqui comigo. Seja bem vindo ao simulador de universos da Locobots.";
+    string path = "./Text/lancamento_vertical_dialog.csv";
+    setencas = csvfile.ReadCSVFile(path);
+    setencas.Add(returnQuestion());
 
-    setencas[1] = "Você escolheu a simulação de lançamento vertical. Graças às atualizações em  nossos sistema, esse mundo não possui atrito no ar, e sua gravidade é de 10 m/s², um ótimo mundo  para se fazer experimento, Dê uma explorada em nossa linda paisagem ";
-
-    setencas[2] = "(Colocar teoria do lançamento vertical, curiosidades)";
-
-    setencas[3] = "Agora que você já aprendeu como funciona o lançamento vertical, vamos testar o seu conhecimento! Seu objetivo nessa fase é chegar até o nosso teletransportador, Essa máquina brilhante aqui do meu lado.";
-
-    setencas[4] = "Ops, parece que seu teletransportador mudou de posição";
-
-    setencas[5] = "Cuidado! Meus sensores indicam a aproximação de inimigos. Você terá três tentativas.";
-
-    setencas[6] = returnQuestion();
-
-    return setencas;
+    return setencas.ToArray();
   }
 
   string returnQuestion()
   {
-    int tam = 3;
 
-    string[] question = new string[tam];
-    int index = Random.Range(0, tam);
+    string path = "./Text/lancamento_exemple.csv";
+    List<string> allquestion = csvfile.ReadCSVFile(path);
+    int indice = Random.Range(0, allquestion.Count - 1);
 
-    question[0] = "De acordo com meu relatório, o centro do teletransportador está a uma distância e tempo de " + distaceDelta.ToString("0.00") + " metros e " + time.ToString("0.00") + " segundos de você";
+    string question = allquestion[indice];
 
-    question[1] = "De acordo com meu relatório, você atingirá a altura máxima, " + maxHeight.ToString("0.00") + " metros, em " + (time / 2).ToString("0.00") + " segudos.";
-
-    question[2] = "De acordo com meu relatório, o ângulo necessário para atingir o teletransportador é de " + angle.ToString() + "graus e a velocidade horizontal de " + velocityX.ToString() + "metros";
-
-    return question[index];
+    return question.Replace("{t}", time.ToString("0.00"))
+    .Replace("{dx}", distaceDelta.ToString("0.00"))
+    .Replace("{vx}", velocityX.ToString("0.00"))
+    .Replace("{vy}", velocityY.ToString("0.00"))
+    .Replace("{mh}", maxHeight.ToString("0.00"));
   }
 }
---
+++ MenuFase/Assets/src/Script/SpecialFuction/CSVfile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO; 
+
+public class CSVfile : MonoBehaviour
+{
+  public List<string> ReadCSVFile(string path)
+  {
+    List<string> textFile = new List<string>();
+    StreamReader streamReader = new StreamReader(path);
+
+    using (streamReader)
+    {
+      while (true)
+      {
+
+        string data = streamReader.ReadLine();
+
+        if (data == null)
+        {
+          break;
+        }
+
+        data = data.Replace("\"", "");
+        textFile.Add(data);
+      }
+    }
+
+    return textFile;
+
+  }
+}
