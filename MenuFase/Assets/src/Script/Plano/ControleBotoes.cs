cb1e24445e06e704575af69481e5b1f6a6e8119a
LucasNovais <lucasnovais222@gmail.com>
2021-02-18 14:07:50 -0300

    Iniciando a finalizacao do plano inclinado.

---
+++ MenuFase/Assets/src/Script/Plano/ControleBotoes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControleBotoes : MonoBehaviour
+{
+    public GameObject menu;
+
+    void Start() {}
+
+    void Update() {}
+
+    public void AbreMenu()
+    {
+        menu.SetActive(true);
+    }
+
+    public void FechaMenu()
+    {
+        menu.SetActive(false);
+    }
+}
